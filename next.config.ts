66f4319eda14a121674d4d9d68a887dd7a7f270f
rong <darong6126@gmail.com>
2026-03-16 11:38:42 +0700

    fix: deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          
-  basePath: "/portfolio1",   
+  output: "export",
   images: {
-    unoptimized: true,       
+    unoptimized: true,
   },
   turbopack: {},
   transpilePackages: [
