57509efd968019848243f3c516782daad4cd8dda
rong <darong6126@gmail.com>
2026-03-16 11:03:47 +0700

    fix: next.contfig.ts

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.glb': {
-          loaders: ['@svgr/webpack'],
-          as: '*.svg',
-        },
-      },
-    },
-  },
-  webpack: (config, { isServer }) => {
-    // Only apply webpack config when not using Turbopack
-    if (!config.experiments?.layers) {
-      config.module.rules.push({
-        test: /\.(glb|gltf)$/,
-        type: 'asset/resource',
-      });
-    }
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/i,
+      type: "asset/resource",
+    });
     return config;
   },
-  transpilePackages: ['three', 'meshline', '@react-three/fiber', '@react-three/drei', '@react-three/rapier']
+  transpilePackages: [
+    "three",
+    "meshline",
+    "@react-three/fiber",
+    "@react-three/drei",
+    "@react-three/rapier",
+  ],
 };
 
 export default nextConfig;
